4d62968a64f8c8c33dd2f11e29571152271feaea
Cap-tain21 <onlycaptains07@gmail.com>
2026-05-05 13:13:27 +0530

    final clean push

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    disabled: true
-  },
-  build: {
-    target: "esnext"
-  }
+  server: { host: true }
 })
